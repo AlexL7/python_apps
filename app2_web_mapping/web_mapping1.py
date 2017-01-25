93b0c846013aff1625e122107d88575ec46fb2c1
Alexander <alexander.lock7@gmail.com>
2017-01-25 16:58:18 +0000

    marker color changes based on elevation

--- app2_web_mapping/web_mapping1.py
+++ app2_web_mapping/web_mapping1.py
@@ -8,10 +8,22 @@ df=pandas.read_csv("Volcanoes-USA.txt") #dataframe object
 #creates map object
 map = folium.Map(location=[45.372,-121.697], zoom_start =6,tiles='Stamen Terrain')
 
+#function to give color based on elevation
+def color(elev):
+    if elev in range (0,1000):
+      col='blue'
+    elif elev in range (1000,2000):
+      col='green'
+    elif elev in range(2000,3000):
+      col='orange'
+    else:
+      col='red'
+    return col
+
 
 # use loop to create a marker for each volacano
-for lat,lon,name in zip(df['LAT'],df['LON'],df['NAME']):
-    map.simple_marker(location=[lat,lon],popup=name, marker_color='red')
+for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
+    map.simple_marker(location=[lat,lon],popup=name, marker_color=color(elev))
 
 
 #manual add markers on the map
